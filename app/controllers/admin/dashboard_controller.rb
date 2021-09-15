58d94ac12bc1f8bf5622f9b0984afe3a92a8a5a4
Dianne <mdianne.gabriel@gmail.com>
2021-09-15 15:40:03 -0400

    Create user mode

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV['HTTP_BASIC_USERNAME'], :password => ENV['HTTP_BASIC_PASSWORD']
-
+  
   def show
     @product_count = Product.count
     @category_count = Category.count
