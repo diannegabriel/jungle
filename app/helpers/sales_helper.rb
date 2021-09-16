5f92e0099148503dbfd1ceeb4e952f39c5074efd
Dianne <mdianne.gabriel@gmail.com>
2021-09-16 19:11:34 -0400

    Modify sales logic so the current sale displays

--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -2,4 +2,12 @@ module SalesHelper
   def active_sale?
     Sale.active.any?
   end
+
+  def sale_title
+    Sale.title
+  end
+
+  def sale_percent
+    Sale.discount
+  end
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -5,6 +5,16 @@ class Sale < ActiveRecord::Base
     Date.current, Date.current)
   end
 
+  def self.title
+    title = Sale.find_by("sales.starts_on <= ? AND sales.ends_on >= ?", 
+    Date.current, Date.current).name
+  end
+
+  def self.discount
+    discount = Sale.find_by("sales.starts_on <= ? AND sales.ends_on >= ?", 
+    Date.current, Date.current).percent_off
+  end
+
   def finished?
     ends_on < Date.current
   end
@@ -16,4 +26,10 @@ class Sale < ActiveRecord::Base
   def active?
     !upcoming? && !finished?
   end
+
+  validates :name, presence: true
+  validates :percent_off, presence: true
+  validates :starts_on, presence: true
+  validates :ends_on, presence: true
+
 end
