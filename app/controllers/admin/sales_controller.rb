4a724816e45649edb45db6ec128a0931be157511
Dianne <mdianne.gabriel@gmail.com>
2021-09-15 21:31:05 -0400

    Get the new sale form working

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -5,4 +5,28 @@ class Admin::SalesController < ApplicationController
     @sales = Sale.all
   end
 
+  def new
+    @sale = Sale.new
+  end
+
+  def create
+    @sale = Sale.new(sale_params)
+
+    if @sale.save
+      redirect_to [:admin, :sales], notice: 'sale created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def sale_params
+    params.require(:sale).permit(
+      :name,
+      :starts_on,
+      :ends_on,
+      :percent_off
+    )
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,4 +38,8 @@ class ApplicationController < ActionController::Base
     redirect_to '/login' unless current_user
   end
 
+  def sale
+    @sales = Sale.all
+  end
+
 end
