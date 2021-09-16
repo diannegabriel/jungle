53d5fe37a0fba08770849d1daaebb108feda9a0c
Dianne <mdianne.gabriel@gmail.com>
2021-09-15 20:51:56 -0400

    Apply the sale to show the front page

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -1,4 +1,5 @@
 class Admin::SalesController < ApplicationController
+  http_basic_authenticate_with :name => ENV['HTTP_BASIC_USERNAME'], :password => ENV['HTTP_BASIC_PASSWORD']
 
   def index
     @sales = Sale.all
